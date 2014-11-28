04f4c767b121910291ac72d684d6ce14a76bb96f
Alex Savchenko <a.savchenko@codetiburon.com>
2014-11-28 17:05:52 +0200

    added factory class to allow caching

--- config/module.config.php
+++ config/module.config.php
@@ -6,12 +6,7 @@ return array(
             'Zf2EmailService\Email' => 'Zf2EmailService\Service\Email',
         ),
         'factories' => array(
-            'Zend\Mail\Transport\Smtp' => function($sm) {
-                $config = $sm->get('Config');
-                $options = isset($config['smtp']) ? new \Zend\Mail\Transport\SmtpOptions($config['smtp']) : array();
-
-                return new \Zend\Mail\Transport\Smtp($options);
-            }
+            'Zend\Mail\Transport\Smtp' => 'Zf2EmailService\Factory\TransportFactory'
         ),
     ),
 );
